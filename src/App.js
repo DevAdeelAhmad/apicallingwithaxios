ae19ddfa6c003ceec165b3a6de03eb9ea4f16d6c
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-07-03 16:33:39 +0500

    comment on fetch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 function App() {
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/todos")
+    fetch("https://jsonplaceholder.typicode.com/todos") //1. Make request  2.Return res as it is  3.Convert it to JSON
       .then((response) => response)
       .then((e) => e.json())
       .then((response) => console.log(response));
