5a304ecd8bc02ea94a51f89cf0210eb85aad1c4d
DevAdeelAhmad <fa20-bse-031@cuilahore.edu.pk>
2023-07-03 16:32:07 +0500

    api call using fetch and useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response)
+      .then((e) => e.json())
+      .then((response) => console.log(response));
+  }, []);
+  return <div className="App"></div>;
 }
 
 export default App;
